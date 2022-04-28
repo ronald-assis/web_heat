7beb594abe30d82f38e03a7fc39af81afe2903de
Ronald Assis <ronaldassis71@gmail.com>
2022-04-28 17:21:19 -0300

    fix: Authorization github and create contexts api

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,13 +1,49 @@
 // import 'dotenv/config';
 
+import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    name: string;
+    github_id: number;
+    avatar_url: string;
+    login: string;
+  }
+}
 
 export function LoginBox() {
   //const CLIENT_ID = process.env.CLIENT_ID;
   const CLIENT_ID = '7d14d98817eeb35ba9bf';
   const signInUrl =`https://github.com/login/oauth/authorize?scope=user&client_id=${CLIENT_ID}`
 
+  const signIn = async (githubCode: string) => {
+   const response = await api.post<AuthResponse>('authenticate',{
+      code: githubCode,
+    }) 
+
+    const {token, user} = response.data;
+    localStorage.setItem('token', token);
+    console.log(user);
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutcode, githubCode] = url.split('?code=');
+
+      window.history.pushState({}, '', urlWithoutcode);
+
+      signIn(githubCode);
+    }
+
+  },[]);
   return (
     <div className={styles.loginBoxWrappe}>
       <strong>Entre e compartilhe sua messegem</strong>
