93b464ea232d904414e27e8c9ad5e07f31fb71b8
Ronald Assis <ronaldassis71@gmail.com>
2022-04-29 18:29:44 -0300

    feat/fix: Created contextApi end styles SendMessageForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { useContext } from 'react';
 import styles from './App.module.scss';
 import { LoginBox } from './components/LoginBox';
 import { MessageList } from './components/MessageList';
+import { SenMessageForm } from './components/SendMessageForm';
+import { AuthContext } from './contexts/auth';
 
 export function App() {
-
+  const { user } = useContext(AuthContext);
   return (
     <main className={styles.contentWrapp}>
       <MessageList />
-      <LoginBox />
+      { !!user ? <SenMessageForm /> : <LoginBox /> }
     </main>
   )
 }
--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,15 +1,86 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import {api}  from '../services/api';  
 
-export const AuthContext = createContext(null);
+type User = {
+  id: string;
+  name: string;
+  avatar_url: string;
+  login: string;
+}
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+  signOut: () => void;
+}
+
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    name: string;
+    github_id: number;
+    avatar_url: string;
+    login: string;
+  }
+}
+
+export const AuthContext = createContext({} as AuthContextData);
 
 type AuthProvider = {
   children: ReactNode;
 }
 
 export function AuthProvider(props: AuthProvider){
+  const [user, setUser] = useState<User | null>(null);  
+  //const CLIENT_ID = process.env.CLIENT_ID;
+  const CLIENT_ID = '7d14d98817eeb35ba9bf';
+  const signInUrl =`https://github.com/login/oauth/authorize?scope=user&client_id=${CLIENT_ID}`
+
+  const signIn = async (githubCode: string) => {
+   const response = await api.post<AuthResponse>('authenticate',{
+      code: githubCode,
+    }) 
+
+    const {token, user} = response.data;
+    localStorage.setItem('token', token);
+    setUser(user);
+  }
+
+  const signOut = () => {
+    setUser(null);
+    localStorage.removeItem('token');
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutcode, githubCode] = url.split('?code=');
+
+      window.history.pushState({}, '', urlWithoutcode);
+
+      signIn(githubCode);
+    }
+
+  },[]);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token'); 
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`; 
+
+      api.get<User>('profile').then((response) => {
+        setUser(response.data);
+      })
+    }
+  },[])
+
   return (
-    <AuthContext.Provider value={null}>
-      {props.children}
+    <AuthContext.Provider value={{ signInUrl, user, signOut}}> {props.children}
     </AuthContext.Provider>
   )
 }
